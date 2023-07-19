858603a2d3b352d4d0231cf03a73571a29178c97
PC-VITOR\vitor <vitor27melo@gmail.com>
2023-07-18 23:00:07 -0300

    finised designing menu

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,36 @@
 <template>
   <div id="canvas">
     <div id="deep-space" />
+    <div id="menu" v-show="isGameRunning()">
+      <button class="button-53" role="button" @click="resumeGame()">Começar / Resumir</button>
+      <div style="height: 15px"/>
+      <button class="button-53" role="button" @click="toggleScoreBoard()">Placar</button>
+      <div style="height: 15px"/>
+      <button class="button-53" role="button" @click="exitGame()">Sair</button>
+      <div style="height: 15px"/>
+      <div id="score-board" v-show="scoreBoardStatus()">
+        <ol>
+          <li>
+            Id da partida: {{scoreBoard[0].matchId}} <br>
+            Início da partida: {{scoreBoard[0].startTime}} <br>
+            Asteróides destruídos: {{scoreBoard[0].nAsteroidsDestroyed}} <br>
+            Score: {{scoreBoard[0].score}} <br>
+          </li><br>
+          <li>
+            Id da partida: {{scoreBoard[1].matchId}} <br>
+            Início da partida: {{scoreBoard[1].startTime}} <br>
+            Asteróides destruídos: {{scoreBoard[1].nAsteroidsDestroyed}} <br>
+            Score: {{scoreBoard[1].score}} <br>
+          </li><br>
+          <li>
+            Id da partida: {{scoreBoard[2].matchId}} <br>
+            Início da partida: {{scoreBoard[2].startTime}} <br>
+            Asteróides destruídos: {{scoreBoard[2].nAsteroidsDestroyed}} <br>
+            Score: {{scoreBoard[2].score}} <br>
+          </li>
+        </ol>
+      </div>
+    </div>
     <div id="space-field">
       <SpaceObject id="spaceship" class="spaceship" :data="spaceField.ship" resolution="2" />
 
@@ -20,6 +50,55 @@
 </template>
 
 <script setup>
+import {ref} from 'vue'
+
+let gameIsRunning = ref(false)
+let showScoreBoard = ref(false)
+let scoreBoard = [
+  {
+    matchId: 0,
+    startTime: "2023-07-18",
+    nAsteroidsDestroyed: 17,
+    score: 170
+  },
+  {
+    matchId: 2,
+    startTime: "2023-07-18",
+    nAsteroidsDestroyed: 10,
+    score: 100
+  },
+  {
+    matchId: 6,
+    startTime: "2023-07-19",
+    nAsteroidsDestroyed: 2,
+    score: 20
+  }
+]
+
+async function toggleScoreBoard() {
+  await $post("/score-board")
+  showScoreBoard = !showScoreBoard
+}
+
+function scoreBoardStatus() {return showScoreBoard}
+
+async function sendPauseCommand() {
+  const command = "PAUSE_GAME";
+  console.log(`Triggering command: ${command}`);
+  await $post("/ship/commands", { command })
+}
+
+function resumeGame() {
+  gameIsRunning = !gameIsRunning
+  sendPauseCommand()
+}
+
+async function exitGame() {
+  await $post("/end-game")
+}
+
+function isGameRunning() {return gameIsRunning}
+
 const {
   data: spaceField,
   refresh: updateSpaceField
@@ -27,6 +106,10 @@ const {
 
 onMounted(() => {
   window.addEventListener("keydown", async (event) => {
+    if (event.code == "Escape") {
+      gameIsRunning = !gameIsRunning
+      await $post("/score-board")
+    }
     const keyToCommand = {
       "ArrowUp": "MOVE_SHIP_UP",
       "ArrowDown": "MOVE_SHIP_DOWN",
@@ -47,9 +130,27 @@ onMounted(() => {
 
   window.setInterval(updateSpaceField, 1000);
 })
+
 </script>
 
 <style>
+#menu {
+  position: relative;
+  top: 10%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 10;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+
+#score-board {
+  position: relative;
+  color: white;
+}
+
 #canvas {
   height: calc(100vh - 4rem);
   width: calc(100vw - 4rem);
@@ -111,5 +212,59 @@ onMounted(() => {
 
 .explosion {
   background-image: url("~/assets/explosion.png");
+}
+
+
+/* FONTE https://getcssscan.com/css-buttons-examples */
+.button-53 {
+  background-color: #3DD1E7;
+  border: 0 solid #E5E7EB;
+  box-sizing: border-box;
+  color: #000000;
+  display: flex;
+  font-family: ui-sans-serif,system-ui,-apple-system,system-ui,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+  font-size: 1rem;
+  font-weight: 700;
+  justify-content: center;
+  line-height: 1.75rem;
+  padding: .75rem 1.65rem;
+  position: relative;
+  text-align: center;
+  text-decoration: none #000000 solid;
+  text-decoration-thickness: auto;
+  width: 100%;
+  max-width: 460px;
+  position: relative;
+  cursor: pointer;
+  transform: rotate(-2deg);
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+}
+
+.button-53:focus {
+  outline: 0;
+}
+
+.button-53:after {
+  content: '';
+  position: absolute;
+  border: 1px solid #000000;
+  bottom: 4px;
+  left: 4px;
+  width: calc(100% - 1px);
+  height: calc(100% - 1px);
+}
+
+.button-53:hover:after {
+  bottom: 2px;
+  left: 2px;
+}
+
+@media (min-width: 768px) {
+  .button-53 {
+    padding: .75rem 3rem;
+    font-size: 1.25rem;
+  }
 }
 </style>
